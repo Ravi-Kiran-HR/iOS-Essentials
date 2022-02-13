966b81dec4ba11aef9bd28d4112f513d925e82aa
Ravi Kiran HR <ravi.kiran@philips.com>
2022-02-13 19:47:18 +0530

    Initial commit - Empty Project Created

---
+++ iOS-Essentials/iOS-Essentials/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-Essentials
+//
+//  Created by Ravi Kiran HR on 13/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
